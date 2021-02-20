77ab749ee84d3f4ccd7c98f37541008cc6fd08be
Conrad Mukai <conrad@mukai-home.net>
2021-02-19 18:29:44 -0800

    Add descriptions to the example outputs.

--- examples/rube-goldberg-alarm-clock/outputs.tf
+++ examples/rube-goldberg-alarm-clock/outputs.tf
@@ -4,8 +4,10 @@
 
 output lambda_function {
   value = module.example.lambda_function
+  description = "name of Lambda function checking time"
 }
 
 output sns_topic {
   value = module.example.sns_topic
+  description = "SNS topic receiving the alarm"
 }
